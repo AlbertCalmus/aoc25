90064b43dd98788b6797508669fb5ce8b5069c47
Damien Avequin <damien@tilt-energy.com>
2025-12-03 13:21:51 +0100

    feat: optimise day2 parce que c'était vraiment la hshouma

--- days/day02.py
+++ days/day02.py
@@ -13,18 +13,24 @@ def generate_more_invalid_ids(max_half: int, max_length: int) -> list[int]:
 def part1(data: str) -> int:
     intervals = [tuple(map(int, interval.split("-"))) for interval in data.split(",")]
     result = 0
+    invalid_ids = sorted(generate_invalid_ids(99999))
     for start, end in intervals:
-        for invalid_id in generate_invalid_ids(99999):
+        for invalid_id in invalid_ids:
             if invalid_id >= start and invalid_id <= end:
                 result += invalid_id
+            elif invalid_id > end:
+                break
     return result
 
 
 def part2(data: str) -> int:
     intervals = [tuple(map(int, interval.split("-"))) for interval in data.split(",")]
     result = 0
+    invalid_ids = sorted(generate_more_invalid_ids(99999, 10))
     for start, end in intervals:
-        for invalid_id in generate_more_invalid_ids(99999, 10):
+        for invalid_id in invalid_ids:
             if invalid_id >= start and invalid_id <= end:
                 result += invalid_id
+            elif invalid_id > end:
+                break
     return result
